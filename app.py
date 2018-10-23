d969a6d022834f5f11c2bb63e485529b6d3b7b83
Alec Burtner-Abt <burtner_abt_alec@lilly.comy>
2018-10-23 11:25:47 -0400

    First section done

--- app.py
+++ app.py
@@ -4,11 +4,39 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/hello')
+app.config['DEBUG'] = True
 
+
+@app.route('/test/<search_query>')
+def search(search_query):
+	return search_query
+
+@app.route('/integer/<int:value>')
+def int_type(value):
+	print(value+1)
+	return 'correct'
+
+@app.route('/float/<float:value>')
+def float_type(value):
+	print(value+1)
+	return 'correct'
+
+@app.route('/path/<path:value>')
+def path_type(value):
+	print(value)
+	return 'corret'
+
+@app.route('/name/<name>')
+def index(name):
+	if name.lower() == 'michael':
+		return f'Hello, {name}',200
+	else:
+		return f'Hello, {name}', 200
+
+
+@app.route('/hello')
 def hello_world():
-	return('Hello, World!')
+	return('Hello, World!?!?')
 
 if __name__=='__main__':
 	app.run()
